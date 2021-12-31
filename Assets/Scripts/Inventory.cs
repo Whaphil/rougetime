d3c2063fe6f814c380cc3d4e893650064e393545
Philipp Sommer <p.sommer@smart-iot.solutions>
2021-12-31 18:26:57 +0100

    inventory now accepts items

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -7,7 +7,16 @@ public class Inventory : MonoBehaviour{
     [SerializeField]
     private bool open = false;
 
+    private List<Item> items = new List<Item>();
+    public IList<Item> Items {
+        get {
+            return items.AsReadOnly();
+        }
+    }
 
 
+    public void AddItem(Item item){
+        items.Add(item);
+    }
 
 }
